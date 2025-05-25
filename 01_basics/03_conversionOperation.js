5d21a54dac1e76dc08922ec13643d6da78421be1
rahul-2515 <yadavrahul200615@gmail.com>
2025-05-25 09:42:47 +0000

    conversion to datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,23 @@
+let score = "rahul"
+console.log(typeof score)
+console.log(score)
+
+let valInNumber = Number(score)
+console.log(typeof valInNumber)
+console.log(valInNumber)
+
+let isLoggedIn = "rahul"
+console.log(typeof isLoggedIn )
+console.log(isLoggedIn)
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanLoggedIn )
+console.log(booleanLoggedIn)
+
+let someNum = 33
+console.log(typeof someNum)
+console.log(someNum)
+
+let stringNumber = String(someNum)
+console.log(typeof stringNumber )
+console.log(stringNumber)
